0499db35bdb5010336966faf3bb84d0f1ceae7af
Sara Gochez <saragochez@Saras-MacBook-Air.local>
2026-03-22 19:16:24 -0500

    added artists list

---
+++ innovative-web-dev/favorite-artists-wk2/js/app.js
@@ -0,0 +1,22 @@
+const artists = [
+    {
+        name: "Olivia Dean",
+        bestSong: "Man I Need",
+        dob: "March 14, 1999"
+    },
+    {
+        name: "Olivia Rodrigo",
+        bestSong: "Lacy",
+        dob: "February 20, 2003"
+    },
+    {
+        name: "Liv Romano",
+        bestSong: "Lasso the Moon",
+        dob: "June 20, 2003"
+    },
+    {
+        name: "Taylor Swift",
+        bestSong: "August",
+        dob: "December 13, 1989"
+    }
+];
